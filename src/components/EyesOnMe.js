bc58dd887188f58fb222c270c79c0291dc8a6190
Leslie Kogi <lesliekogi@gmail.com>
2025-04-16 20:56:25 +0300

    events

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+
+ function EyesOnMe() {
+    function display(){
+     console.log("Good!");
+    }
+    function display2(){
+        console.log("Hey! Eyes on me!")
+    }
+  return (
+    <div>
+      <button onFocus={display} onBlur={display2}>Eyes on me</button>
+    </div>
+  )
+}
+export default EyesOnMe
+
